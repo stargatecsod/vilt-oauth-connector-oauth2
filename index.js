c822c1523374c1855664039930744546f56ddc46
Ajay Tilekar <atilekar@csod.com>
2026-02-21 12:45:05 +0530

    added code in middleware

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  const match = req.url.match(/^\/api\/[^/]+(\/.*)$/);
+
+  if (match) {
+    req.url = "/api" + match[1];
+  }
+
+  next();
+});
+
 // Health
 app.get("/health", (_req, res) => res.json({ ok: true }));
 
